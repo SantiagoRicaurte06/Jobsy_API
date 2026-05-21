5c593fa0369f157c544e0ef221a0f80f3b73edf4
VICTOR-011199 <victoreduardomarinovega@gmail.com>
2026-05-21 14:01:39 -0500

    Se arregla en archivo main

--- Usuarios_Api/main.go
+++ Usuarios_Api/main.go
@@ -2,22 +2,58 @@ package main
 
 import (
 	_ "JOBSY_API/routers"
+	"fmt"
 
-	beeLogger "github.com/beego/bee/v2/logger"
+	"github.com/beego/beego/v2/server/web/filter/cors"
+
 	"github.com/beego/beego/v2/client/orm"
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
+
 func main() {
-	sqlConn, err := beego.AppConfig.String("sqlconn")
+
+    err := godotenv.Load()
+    if err != nil {
+        panic("Error cargando el archivo .env: " + err.Error())
+    }
+
+	//regargar app.config despues de cargar .env
+	err = beego.LoadAppConfig("ini", "conf/app.conf")
 	if err != nil {
-		beeLogger.Log.Fatal(err.Error())
+		panic(err)
 	}
-	orm.RegisterDataBase("default", "postgres", sqlConn)
-	if beego.BConfig.RunMode == "dev" {
+
+    User, _    := beego.AppConfig.String("PGuser")
+    password, _ := beego.AppConfig.String("PGpassword")
+    host,    _ := beego.AppConfig.String("PGhost")
+    port,     _ := beego.AppConfig.String("PGport")
+    database, _ := beego.AppConfig.String("PGdatabase")
+    schema, _ := beego.AppConfig.String("PGschema")
+
+	fmt.Printf("postgresSqL connection string: postgres://%s:%s@%s:%s/%s?sslmode=disable&search_path="+"%s\n", User, password, host, port, database, schema)
+
+
+// Register the database connection
+orm.RegisterDataBase("default", "postgres", "postgres://"+User+":"+password+"@"+host+":"+port+"/"+database+"?sslmode=disable&search_path="+schema)
+
+// config swagger	
+	if  beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+// config cors
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: 		[]string{"*"},
+		AllowMethods: 		[]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: 		[]string{"Origin","x-requested-with","content-type","accept","origin","authorization","x-csrftoken",},
+		ExposeHeaders:   	[]string{"Content-Length"},
+		AllowCredentials: 	true,
+	}))
+
+// run the server
 	beego.Run()
 }
